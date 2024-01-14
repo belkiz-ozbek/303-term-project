49fcd360b31f8c1a1a8843d7d4917618b5a238e5
Beyza Karakurt <125734176+KarakurtBeyza@users.noreply.github.com>
2024-01-14 17:54:18 +0300

    Update Ceng303-TermProject.c
    gerekenleri en başa commentledim

--- Ceng303-TermProject.c
+++ Ceng303-TermProject.c
@@ -1,31 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include <time.h>
 #define MAX_CLASSES 100
 #define MAX_CLASSROOMS 50
 #define MAX_BLOCKED_HOURS 30
+#define START_TIME 540   // 9:00 AM in minutes (9 * 60)
+#define END_TIME 1080    // 6:00 PM in minutes (18 * 60)
+#define TIME_SLOT 30  
 
-/// her student ve prof için availablity-time structure(?) lazım
-/// 
+//ramdomness ortadan kaldırılabilirse sonuç bulma şansımız daha çok!!!!!
+//dakika sasat dönüşümlerinden emin değilim dikkatlice incelnemsi gerek
+//available methodlarını kullanarak checkSchedule yazmaya çalıştım olmadı ama yine de silmedim isterseniz siz de deneyebilirsiniz
 
-typedef struct {
-    char prof_name[20];
-    int is_available; // 1 if available, 0 if not
-} ProfessorAvailability;
-
-typedef struct {
-    int student_id;
-    int is_available; // 1 if available, 0 if not
-} StudentAvailability;
+const char *weekdays[] = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+const int dayDuration = 540; // 9 hours in minutes
+int numOfDays = 6; // Number of days in a week
+int numOfClasses = 0;
+int numOfClassrooms = 0;
 
 typedef struct {
     int student_id;
     char prof_name[20];
     char course_id[20];
     int exam_duration;
-    int assigned; // 1 if assigned, 0 if not	
-} Classes; //lecture
+    int assigned; // 1 if assigned, 0 if not
+} Classes;
 
 typedef struct {
     char room_id[10];
@@ -38,6 +38,21 @@ typedef struct {
     int endingTime;
 } BlockedHours;
 
+typedef struct {
+    int courseIndex;
+    int roomIndex;
+    char day[9];
+    int startingTime;
+} Schedule;
+
+void getRandomDay(char day[9]) {
+    char days[6][9] = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+    int randomDayIndex = rand() % 6;
+    strcpy(day, days[randomDayIndex]);
+}
+int getRandomStartingTime() {
+    return START_TIME + (rand() % ((END_TIME - START_TIME) / TIME_SLOT)) * TIME_SLOT;
+}
 void getFiles(char arr[], const char *forWhat) {
     printf("Please enter the file name for %s: ", forWhat);
     scanf("%s", arr);
@@ -53,10 +68,8 @@ void readingToClassesStruct(FILE *classesFile, Classes classes[], int *numOfClas
     int classesCount = 0;
     char line[MAX_CLASSES];
     while (fgets(line, sizeof(line), classesFile) != NULL && classesCount < MAX_CLASSES) {
-        // Remove newline character from the end of the line
         line[strcspn(line, "\n")] = '\0';
 
-        // Reading from the file to classes struct
         sscanf(line, "%d,%19[^,],%19[^,],%d",
                &classes[classesCount].student_id, classes[classesCount].prof_name,
                classes[classesCount].course_id, &classes[classesCount].exam_duration);
@@ -76,10 +89,8 @@ void readingToClassroomsStruct(FILE *classroomsFile, Classrooms classrooms[], in
     int classroomsCount = 0;
     char line[MAX_CLASSROOMS];
     while (fgets(line, sizeof(line), classroomsFile) != NULL && classroomsCount < MAX_CLASSROOMS) {
-        // Remove newline character from the end of the line
         line[strcspn(line, "\n")] = '\0';
 
-        // Reading from the file to struct
         sscanf(line, "%9[^,],%d", classrooms[classroomsCount].room_id, &classrooms[classroomsCount].capacity);
 
         classroomsCount++;
@@ -96,23 +107,20 @@ void readingToBlockedHoursStruct(FILE *blockedHoursFile, BlockedHours blockedHou
 
     int blockedHoursCount = 0;
     char line[MAX_BLOCKED_HOURS];
-    
+
     while (fgets(line, sizeof(line), blockedHoursFile) != NULL && blockedHoursCount < MAX_BLOCKED_HOURS) {
-        // Remove newline character from the end of the line
         line[strcspn(line, "\n")] = '\0';
 
-        // Check if the line is empty
         if (line[0] == '\0') {
-            // Skip empty line
             continue;
         }
 
-        // Reading from the file to struct
-        sscanf(line, "%9[^,],%d,%d", blockedHoursArray[blockedHoursCount].day, &blockedHoursArray[blockedHoursCount].startingTime, &blockedHoursArray[blockedHoursCount].endingTime);
+        sscanf(line, "%9[^,],%d,%d", blockedHoursArray[blockedHoursCount].day,
+               &blockedHoursArray[blockedHoursCount].startingTime, &blockedHoursArray[blockedHoursCount].endingTime);
 
         blockedHoursCount++;
     }
-	printf("%d",blockedHoursCount);
+
     *numOfBlockedHours = blockedHoursCount;
     fclose(blockedHoursFile);
 }
@@ -121,7 +129,6 @@ void clearInputBuffer() {
     int c;
     while ((c = getchar()) != '\n' && c != EOF);
 }
-
 
 int compare(const Classrooms *a, const Classrooms *b) {
     return ((Classrooms *)b)->capacity - ((Classrooms *)a)->capacity;
@@ -150,10 +157,11 @@ void Heap(Classrooms ar[], int N, int i) {
 }
 
 void sort(Classrooms ar[], int N) {
-	int i;
+    int i;
     for (i = (N / 2) - 1; i >= 0; i--) {
         Heap(ar, N, i);
     }
+
     for (i = N - 1; i >= 0; i--) {
         Classrooms temp = ar[0];
         ar[0] = ar[i];
@@ -163,194 +171,172 @@ void sort(Classrooms ar[], int N) {
     }
 }
 
-/// Sorting Classes according to exam_duration -> course_id -> student_id
-int partition(Classes ar[], int low, int high){
-	Classes pivot= ar[high];
-	int i = low-1;
-	int j;
-	for(j=low; j<=high-1; j++){
-		if(ar[j].exam_duration <= pivot.exam_duration){
-			if(ar[j].exam_duration == pivot.exam_duration){ //course_id comparison
-				if(strcmp(ar[j].course_id, pivot.course_id)>=0){
-					if(strcmp(ar[j].course_id, pivot.course_id)==0){
-						if(ar[j].student_id < pivot.student_id){
-							i++;
-							Classes temp= ar[i];
-							ar[i]=ar[j];
-							ar[j]=temp;
-							//swap
-						}
-					}
-				}else{
-					i++;
-					Classes temp= ar[i];
-					ar[i]=ar[j];
-					ar[j]=temp;
-					//swap
-				}
-			}
-		}else{
-			i++;
-			Classes temp= ar[i];
-			ar[i]=ar[j];
-			ar[j]=temp;
-			//swap
-		}
-	}
-	
-	Classes temp= ar[i+1];
-	ar[i+1]=ar[high];
-	ar[high]=temp;
-	//swap
-	
-	return i+1;
-}
 
-void quickSort(Classes ar[], int low, int high){
-	if(low<high){
-		int pivot= partition(ar, low, high);
-		quickSort(ar, low, pivot-1);
-		quickSort(ar, pivot+1, high);
-	}
-}
 
-//////
-
-int minutesToHours(Classes ar[], int i){
-	int hour= ar[i].exam_duration / 60;
-	int min= ar[i].exam_duration %60;
-	
-	return min+(hour*100);
-}
-
-void initializeAvailability(StudentAvailability studentAvailabilities[], ProfessorAvailability profAvailabilities[], int numOfStudents, int numOfProfs) {
-    // Initialize student availabilities
-    for (int i = 0; i < numOfStudents; i++) {
-        studentAvailabilities[i].student_id = i + 1; // Assuming student IDs start from 1
-        studentAvailabilities[i].is_available = 1; // Initially, all students are available
-    }
-
-    // Initialize professor availabilities
-    for (int i = 0; i < numOfProfs; i++) {
-        strcpy(profAvailabilities[i].prof_name, ""); // Initialize with an empty string
-        profAvailabilities[i].is_available = 1; // Initially, all professors are available
-    }
-}
-
-int isStudentAvailable(StudentAvailability studentAvailabilities[], int student_id) {
-    return studentAvailabilities[student_id - 1].is_available;
-}
-
-void setStudentUnavailable(StudentAvailability studentAvailabilities[], int student_id) {
-    studentAvailabilities[student_id - 1].is_available = 0;
-}
-
-int isProfAvailable(ProfessorAvailability profAvailabilities[], const char *prof_name) {
-    for (int i = 0; i < MAX_CLASSES; i++) {
-        if (strcmp(profAvailabilities[i].prof_name, prof_name) == 0 && profAvailabilities[i].is_available) {
-            return 1; // Prof is available
+int isClassroomAvailable(Classrooms classroom, int day, int timeSlot, int examDuration, Schedule schedule[], int scheduleSize) {
+    for (int i = 0; i < scheduleSize; i++) {
+        if (strcmp(schedule[i].day, weekdays[day]) == 0 &&
+            schedule[i].startingTime <= timeSlot && timeSlot <= schedule[i].startingTime + examDuration) {
+            return 0;
         }
     }
-    return 0; // Prof is not available
+    return 1;
 }
 
-void setProfUnavailable(ProfessorAvailability profAvailabilities[], const char *prof_name) {
-    for (int i = 0; i < MAX_CLASSES; i++) {
-        if (strcmp(profAvailabilities[i].prof_name, prof_name) == 0) {
-            profAvailabilities[i].is_available = 0; // Mark prof as unavailable
-            break;
+int isStudentAvailable(int studentID, int day, int timeSlot, int examDuration, Schedule schedule[], int scheduleSize, Classes classes[]) {
+    for (int i = 0; i < scheduleSize; i++) {
+        if (strcmp(schedule[i].day, weekdays[day]) == 0 &&
+            schedule[i].startingTime <= timeSlot + examDuration &&
+            timeSlot <= schedule[i].startingTime + classes[schedule[i].courseIndex].exam_duration &&
+            studentID == classes[schedule[i].courseIndex].student_id) {
+            return 0;
         }
     }
+    return 1;
 }
 
+int isProfAvailable(const char *profName, int day, int timeSlot, int examDuration, Schedule schedule[], int scheduleSize, Classes classes[]) {
+    for (int i = 0; i < scheduleSize; i++) {
+        if (strcmp(schedule[i].day, weekdays[day]) == 0 &&
+            schedule[i].startingTime <= timeSlot && timeSlot <= schedule[i].startingTime + examDuration &&
+            strcmp(profName, classes[schedule[i].courseIndex].prof_name) == 0) {
+            return 0;
+        }
+    }
+    return 1;
+}
 
-void setExams(Classes ar[], int index, StudentAvailability studentAvailabilities[], ProfessorAvailability profAvailabilities[], int numOfClasses) {
-    if (index == numOfClasses) {
-        return; // End of the array
+// Function we will  use  in the backtracking step
+int checkSchedule(const Schedule *currentSchedule, const Classes *classes, const Classrooms *classrooms, const BlockedHours *blockedHours, int numScheduledExams) {
+    for (int i = 0; i < numScheduledExams; i++) {
+        // Checkking student availablity
+        if (currentSchedule[i].courseIndex != -1 &&
+            classes[currentSchedule[i].courseIndex].student_id == classes[numScheduledExams].student_id &&
+            strcmp(classes[currentSchedule[i].courseIndex].course_id, classes[numScheduledExams].course_id) != 0) {
+            return 0;  // there şs a conflict ---->Student has two exams at the same time
+        }
+
+        // Checking  professor availablity
+        if (currentSchedule[i].courseIndex != -1 &&
+            strcmp(classes[currentSchedule[i].courseIndex].prof_name, classes[numScheduledExams].prof_name) == 0) {
+            return 0;  // there is a conflict--->Professor has two exams at the same time
+             
+        }
     }
 
-    if (index == 0) {
-        quickSort(ar, 0, numOfClasses - 1);
-        // Check BlockedHours, Professors, and Students availability
-        setExams(ar, index + 1, studentAvailabilities, profAvailabilities, numOfClasses);
-    } else {
-        if (strcmp(ar[index].course_id, ar[index - 1].course_id) != 0 || strcmp(ar[index].prof_name, ar[index - 1].prof_name) != 0) {
-            // Check BlockedHours, Professors, and Students availability
-            setExams(ar, index + 1, studentAvailabilities, profAvailabilities, numOfClasses);
-        } else {
-            // If index-th element is for the same class and professor as the previous one, check availability
-            if (isStudentAvailable(studentAvailabilities, ar[index].student_id) && isProfAvailable(profAvailabilities, ar[index].prof_name)) {
-                // Assign exam and mark student and professor as unavailable for this time slot
-                printf("Assigning exam for Student ID: %d, Prof Name: %s, Course ID: %s, Exam Duration: %d\n",
-                       ar[index].student_id, ar[index].prof_name, ar[index].course_id, ar[index].exam_duration);
-                setStudentUnavailable(studentAvailabilities, ar[index].student_id);
-                setProfUnavailable(profAvailabilities, ar[index].prof_name);
+    // Checking the room capacity 
+    int roomCapacity = classrooms[currentSchedule[numScheduledExams].roomIndex].capacity;
+    if (roomCapacity >= classes[numScheduledExams].exam_duration / 2) {
+        return 0; 
+    }
+
+    // Checking the conflict of blocked hours
+    for (int i = 0; i < numScheduledExams; i++) {
+        if (strcmp(currentSchedule[i].day, blockedHours[numScheduledExams].day) == 0 &&
+            currentSchedule[i].startingTime >= blockedHours[numScheduledExams].startingTime &&
+            currentSchedule[i].startingTime < blockedHours[numScheduledExams].endingTime) {
+            return 0; 
+        }
+    }
+
+    return 1;  // a valid schedule is found!!!!
+}
+
+void printExamSchedule(const Schedule *schedule, const Classes *classes, const Classrooms *classrooms, int numScheduledExams) {
+    printf("Exam Schedule:\n");
+    printf("%-10s %-20s %-10s %-15s %-10s\n", "Day", "Starting Time", "Room", "Prof Name", "Course ID");
+
+    for (int i = 0; i < numScheduledExams; i++) {
+        int classIndex = schedule[i].courseIndex;
+        int roomIndex = schedule[i].roomIndex;
+
+        printf("%-10s %-20d %-10s %-15s %-10s\n",
+               schedule[i].day,
+               schedule[i].startingTime,
+               classrooms[roomIndex].room_id,
+               classes[classIndex].prof_name,
+               classes[classIndex].course_id);
+    }
+}
+
+void backtrackToFindSchedule(Schedule *currentSchedule, const Classes *classes, const Classrooms *classrooms, const BlockedHours *blockedHours, int numScheduledExams, int numCourses, int numRooms, int numBlockedHours) {
+  if (numScheduledExams == numCourses) {
+        printf("all exams are scheduled");
+        return;
+    }
+
+    for (int j = 0; j < numCourses; j++) {
+      
+        if (currentSchedule[j].courseIndex == -1) {
+            for (int i = 0; i < numRooms; i++) {
+                currentSchedule[j].courseIndex = j;
+                currentSchedule[j].roomIndex = i;
+
+                if (checkSchedule(currentSchedule, classes, classrooms, blockedHours, numScheduledExams)) {
+                    getRandomDay(currentSchedule[j].day);
+                    currentSchedule[j].startingTime = getRandomStartingTime();
+
+                    // recursive backtrack
+                    backtrackToFindSchedule(currentSchedule, classes, classrooms, blockedHours, numScheduledExams + 1, numCourses, numRooms, numBlockedHours);
+
+                    //remove assignment to backtrack
+                    currentSchedule[j].courseIndex = -1;
+                    currentSchedule[j].roomIndex = -1;
+                    getRandomDay(currentSchedule[j].day);
+                    currentSchedule[j].startingTime = getRandomStartingTime();
+                } else {
+                    //bu kısımdan emin değilim
+                    currentSchedule[j].courseIndex = -1;
+                    currentSchedule[j].roomIndex = -1;
+                    getRandomDay(currentSchedule[j].day);
+                    currentSchedule[j].startingTime = getRandomStartingTime();
+                }
             }
-            // Check BlockedHours, Professors, and Students availability
-            setExams(ar, index + 1, studentAvailabilities, profAvailabilities, numOfClasses);
         }
     }
+
 }
+
 
 int main() {
     Classes classes[MAX_CLASSES];
     Classrooms classrooms[MAX_CLASSROOMS];
-    BlockedHours blockedHours[MAX_BLOCKED_HOURS];
-    StudentAvailability studentAvailabilities[MAX_CLASSES];
-    ProfessorAvailability profAvailabilities[MAX_CLASSES];
+    BlockedHours blockedHours[MAX_CLASSES];
+    Schedule currentSchedule[MAX_CLASSES];
 
     int numOfClasses = 0;
     int numOfClassrooms = 0;
     int numOfBlockedHours = 0;
+    int numScheduledExams=0;
 
-    // Reading classes file
     char classesFileName[30];
     getFiles(classesFileName, "for classes");
     FILE *classesFile = fopen(classesFileName, "r");
     readingToClassesStruct(classesFile, classes, &numOfClasses);
 
-    // Reading classrooms file
     char classroomsFileName[30];
     getFiles(classroomsFileName, "for classrooms");
     FILE *classroomsFile = fopen(classroomsFileName, "r");
     readingToClassroomsStruct(classroomsFile, classrooms, &numOfClassrooms);
 
-
-    //Reading blocked hours file
     char blockedHoursFileName[30];
     getFiles(blockedHoursFileName, "for blocked hours");
     FILE *blockedHoursFile = fopen(blockedHoursFileName, "r");
-    readingToBlockedHoursStruct(blockedHoursFile,blockedHours,&numOfBlockedHours);
-    
-    // Sorting classrooms
+    readingToBlockedHoursStruct(blockedHoursFile, blockedHours, &numOfBlockedHours);
+
     sort(classrooms, numOfClassrooms);
-
-    // Displaying blocked hours
-    int i;
-    for (i = 0; i < numOfBlockedHours; i++) {
-        printf("Blocked Hours %s %d %d\n", blockedHours[i].day, blockedHours[i].startingTime, blockedHours[i].endingTime);
+    
+     for (int i = 0; i < MAX_CLASSES; i++) {
+        currentSchedule[i].courseIndex = -1;
+        currentSchedule[i].roomIndex = -1;
+        getRandomDay(currentSchedule[i].day);
+        currentSchedule[i].startingTime = getRandomStartingTime();
     }
 
-	//////////////////*********************//////////////
-	//sort according to exam duration -> course id -> student id
-	
-	 initializeAvailability(studentAvailabilities, profAvailabilities, MAX_CLASSES, MAX_CLASSES);
-    setExams(classes, 0, studentAvailabilities, profAvailabilities, numOfClasses);
-
-	
-    // Displaying classes
-    for (i = 0; i < numOfClasses; i++) {
-        printf("student ID: %d, Profname: %s, class ID: %s, time: %d\n",
-               classes[i].student_id, classes[i].prof_name, classes[i].course_id, classes[i].exam_duration);
-    }
-
-    // Displaying classrooms
-    for (i = 0; i < numOfClassrooms; ++i) {
-        printf("Room ID: %s, Capacity: %d\n", classrooms[i].room_id, classrooms[i].capacity);
-    }
-
+    backtrackToFindSchedule(currentSchedule, classes, classrooms, blockedHours, 0, numOfClasses, numOfClassrooms, numOfBlockedHours);
+    printExamSchedule(currentSchedule, classes, classrooms, numScheduledExams);
     fclose(classesFile);
     fclose(classroomsFile);
-
     return 0;
 }
+
